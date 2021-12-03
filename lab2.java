cd4f3bc0bd19a5d550cb12b7a8082c457e26c691
S94481rth <93481116+S94481rth@users.noreply.github.com>
2021-12-03 12:44:27 +0530

    Update lab2.java

--- lab2.java
+++ lab2.java
@@ -1,58 +1,52 @@
 import java.lang.*;
 import java.util.Scanner;
+
 class Main{
     String usn;
     String name;
-    double sgpa;
     int[] credits,scores;
     int number_of_subjects;
-    
-    
+    double sgpa;
     void accept(){
-        Scanner obj = new Scanner(System.in);
-        System.out.println("Enter usn :: ");
-        usn = obj.nextLine();
-        System.out.println("Enter name :: ");
+        Scanner obj = new Scanner(System.in); 
+        System.out.println("Enter name:: ");
         name = obj.nextLine();
-        System.out.println("Enter the number of subjects :: ");
+        System.out.println("Enter USN:: ");
+        usn = obj.nextLine();
+        System.out.println("Enter number of subjects :: ");
         number_of_subjects = obj.nextInt();
-        System.out.println("Enter scores and corresponding credits :: ");
         credits = new int[number_of_subjects];
         scores = new int[number_of_subjects];
-        for(int i=0; i < number_of_subjects ; i++ ){
-            System.out.println("Score of Subject" + (i+1));
+        System.out.println("Enter marks and corresponding credits ::  ");
+        for(int i=0;i<number_of_subjects;i++){
+            System.out.println("subject-" +(i+1));
             scores[i] = obj.nextInt();
-            System.out.println("Credits for Subject" + (i+1));
+            System.out.println("credits-" +(i+1));
             credits[i] = obj.nextInt();
         }
-        
     }
-        
     void calculate(){
-        double total_prod=0, total_credits = 0;
+        double total_credits=0,total_prod=0;
         for(int i=0;i<number_of_subjects;i++){
-            total_credits = credits[i] + total_credits;
-            total_prod = total_prod + credits[i]*scores[i];
+            total_prod = total_prod + scores[i]*credits[i];
+            total_credits = total_credits + credits[i];
         }
-        double sgpa = total_prod / total_credits; 
-        System.out.println(total_credits + " "+ total_prod + "  " + sgpa);
+        double sgpa = total_prod / total_credits;
         display(sgpa);
     }
-    
     void display(double sgpa){
         System.out.println("Name :: " + name);
         System.out.println("USN :: " + usn);
         System.out.println("Scores :: "  );
         for(int i=0;i<number_of_subjects;i++){
-        System.out.print(" " + scores[i] +" "  );
+            System.out.print("subject-"+(i+1)+" :: " + scores[i]);
         }
         System.out.println(" ");
-        System.out.println("The SGPA of the student is :: " + sgpa);
+        System.out.println("GPA of the student is ::  "+ sgpa);
     }
     public static void main(String args[]){
         Main student1 = new Main();
         student1.accept();
         student1.calculate();
-        
-    }
+    } 
 }
