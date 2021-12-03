edc0e71c495150fd7d53b490c6db55655f936458
S94481rth <93481116+S94481rth@users.noreply.github.com>
2021-12-03 22:35:16 +0530

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -1,49 +1,53 @@
-import java.util.Scanner;
 import java.lang.*;
-class lab3{
-    int price;
-    int num_pages;
+import java.util.Scanner; 
+class Main{
     String name;
     String author;
-    lab3() {
-      int price =0;
-      int num_pages=0;
-      String name;
-      String author;   
+    double price;
+    int pages;
+    Main(){
+    name = "";
+    author = "";
+    price = 0 ;
+    pages =0;
     }
+    
     void accept(){
         Scanner obj = new Scanner(System.in);
-        System.out.println("Enter the price and number of pages :  ");
+        System.out.println("Enter the name of the book ::  ");
+        name = obj.nextLine();
+        System.out.println("Enter the name of the author ::  ");
+        author = obj.nextLine();
+        System.out.println("Enter the price of the book ::  ");
         price = obj.nextInt();
-        num_pages = obj.nextInt();
-        System.out.println("Enter book and author:   ");
-        name = obj.next();
-        author = obj.next();
+        System.out.println("Enter the number of pages ::  ");
+        pages = obj.nextInt();
     }
-    void display(){
-        System.out.println("The price of the book is : " + price + " and the number of pages are : " + num_pages);
-        System.out.println("The name of the book is : " + name + " and the name of author is : " + author+"\n");
+    
+    public String toString(){
+        return "\n name of book is " + name + "\n Name of author is :: " +author+ "\n Price OF book is "+ price   + "\n number of pages is " + pages + "\n";   
     }
     public static void main(String args[]){
         Scanner obj = new Scanner(System.in);
-        System.out.println("Enter the number of books: ");
-        int num_books = obj.nextInt();
-        lab3 book[] = new lab3[num_books];
-        for(int i=0;i<num_books;i++){
-            book[i] = new lab3();
+        System.out.println("Enter the number of books :: ");
+        int number_of_books = obj.nextInt();
+        Main book[] = new Main[number_of_books];
+        for(int i=0;i<number_of_books;i++){
+            book[i] = new Main();
             book[i].accept();
         }
-        for(int i=0;i<num_books;i++){
-        book[i].display();
+        for(int i=0;i<number_of_books;i++){
+            System.out.println(book[i]);
         }
-        int large = book[0].price,index = 0 ;
-        for(int i=0;i<num_books;i++){
-            if(large < book[i].price){
-                large = book[i].price;
+        double largest = book[0].price;
+        int index = 0;
+        for(int i=0;i<number_of_books;i++){
+            if(largest < book[i].price){
+                largest = book[i].price;
                 index = i;
             }
         }
-        System.out.println("\nThe details of book with hightest price is : ");
-        book[index].display();
+        System.out.println("Details of book with higest price is given :: ");
+        System.out.println(book[index]);
     }
 }
