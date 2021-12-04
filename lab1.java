c117d3418f4001dbe5e32e7c61d78d200362a58d
S94481rth <93481116+S94481rth@users.noreply.github.com>
2021-12-04 17:19:27 +0530

    Create lab1.java

---
+++ lab1.java
@@ -0,0 +1,30 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class Main {
+  public static void main(String args[]){
+      int a,b,c;
+      Scanner obj = new Scanner(System.in);
+      System.out.println("Enter the coefficients of a,b,c :: ");
+      a = obj.nextInt();
+      b = obj.nextInt();
+      c = obj.nextInt();
+      int d = b*b - 4*a*c;
+      if(d == 0){
+          double root = -b/2*a;
+          System.out.println("The roots are equal and are equal to ::  " + root);
+      }
+      else if(d > 0){
+          double sqrt_d = Math.sqrt(d);
+          double root1 = (sqrt_d - b)/2*a;
+          double root2 = (-sqrt_d - b)/2*a;
+          System.out.println("The roots are :: " + root1 + " and  :: " + root2); 
+      }
+      else if(d<0){
+        double sqrt_d = Math.sqrt(-d);
+        double real_part = (double)b/2*a*(-1);
+        double imag_term = sqrt_d/2*a;
+        System.out.println("The root1 is  :: " + real_part + "+ i*"+ imag_term);
+        System.out.println("The root2 is  :: " + real_part + "- i*"+ imag_term); 
+      }
+    }   
+}
