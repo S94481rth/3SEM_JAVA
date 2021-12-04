a8ec8089c8415dd571ea00d5ee37c6d109b86500
S94481rth <93481116+S94481rth@users.noreply.github.com>
2021-12-04 17:34:46 +0530

    Update lab4.java

--- lab4.java
+++ lab4.java
@@ -1,38 +1,29 @@
-abstract class shape
-{
+abstract class shape{
 int x,y;
 abstract void printArea(double x,double y);
 }
-class Rectangle extends shape
-{
-void printArea(double x,double y)
-{
+class Rectangle extends shape{
+void printArea(double x,double y){
 System.out.println("Area of rectangle is :"+(x*y));
+    }
 }
-}
-class Circle extends shape
-{
-void printArea(double x,double y)
-{
+class Circle extends shape{
+void printArea(double x,double y){
 System.out.println("Area of circle is :"+(3.14*x*x));
+    }
 }
-}
-class Triangle extends shape
-{
-void printArea(double x,double y)
-{
+class Triangle extends shape{
+void printArea(double x,double y){
 System.out.println("Area of triangle is :"+(0.5*x*y));
+    }
 }
-}
-public class Main
-{
-public static void main(String[] args)
-{
+public class Main{
+public static void main(String[] args){
 Rectangle r=new Rectangle();
 r.printArea(4,10);
 Circle c=new Circle();
 c.printArea(3,5);
 Triangle t=new Triangle();
 t.printArea(4,7);
-}
+    }
 }
